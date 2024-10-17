e108547268753c3c3287f56a151ef2dbc0134e65
Zebreus <zebreus@zebre.us>
2024-10-17 21:59:41 +0200

    Improve cli

--- rudelblinken-cli/src/main.rs
+++ rudelblinken-cli/src/main.rs
@@ -28,11 +28,11 @@ where
     adapter.set_powered(true).await?;
 
     {
-        println!(
-            "Discovering on Bluetooth adapter {} with address {}\n",
-            adapter.name(),
-            adapter.address().await?
-        );
+        // eprintln!(
+        //     "Discovering on Bluetooth adapter {} with address {}\n",
+        //     adapter.name(),
+        //     adapter.address().await?
+        // );
         let (abort_handle, abort_registration) = AbortHandle::new_pair();
         let discover = adapter.discover_devices().await?;
         pin_mut!(discover);
@@ -48,7 +48,6 @@ where
                     let device = adapter.device(addr)?;
                     let result = f(device).await;
                     if result.is_ok() {
-                        println!("is ok");
                         programmed_devices += 1;
                     }
                     if programmed_devices >= max_devices {
@@ -61,7 +60,6 @@ where
                 _ => (),
             }
         }
-        println!("Stopping discovery");
     }
 
     return Ok(());
@@ -77,6 +75,19 @@ struct Cli {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    /// Upload a file
+    Upload {
+        /// Stop scanning after this many seconds
+        #[arg(short, long, default_value = "5")]
+        timeout: f32,
+
+        /// Maximum number of devices to program
+        #[arg(short, long, default_value = "1")]
+        devices: u32,
+
+        /// WASM file that will get flashed to the devices
+        file: PathBuf,
+    },
     /// Run a WASM binary
     Run {
         /// Stop scanning after this many seconds
@@ -103,45 +114,89 @@ async fn main() -> bluer::Result<()> {
     env_logger::init();
     let cli = Cli::parse();
 
-    let Commands::Run {
-        timeout,
-        devices,
-        file,
-    } = cli.command
-    else {
-        todo!();
+    match cli.command {
+        Commands::Upload {
+            timeout,
+            devices,
+            file,
+        } => {
+            let file_content = tokio::fs::read(file)
+                .await
+                .expect("Failed to read the WASM file");
+
+            scan_for(
+                Duration::from_millis((timeout * 1000.0) as u64),
+                devices,
+                &async |device| -> Result<(), UpdateTargetError> {
+                    let update_target = UpdateTarget::new_from_peripheral(&device).await?;
+
+                    let data = &file_content;
+
+                    let now = Instant::now();
+                    update_target.upload_file(&data).await?;
+                    let duration = now.elapsed();
+                    println!(
+                        "Sending {}k took {} millis",
+                        data.len() / 1024,
+                        duration.as_millis()
+                    );
+                    println!(
+                        "Thats {}kb/s",
+                        (data.len() as f64 / duration.as_millis() as f64)
+                    );
+                    return Ok(());
+                    // update_target.device.disconnect().await.unwrap();
+                },
+            )
+            .await
+            .unwrap();
+        }
+        Commands::Run {
+            timeout,
+            devices,
+            file,
+        } => {
+            let file_content = tokio::fs::read(file)
+                .await
+                .expect("Failed to read the WASM file");
+
+            scan_for(
+                Duration::from_millis((timeout * 1000.0) as u64),
+                devices,
+                &async |device| -> Result<(), UpdateTargetError> {
+                    let update_target = UpdateTarget::new_from_peripheral(&device).await?;
+
+                    let data = &file_content;
+
+                    let now = Instant::now();
+                    update_target.run_program(&data).await?;
+                    return Ok(());
+                    // update_target.device.disconnect().await.unwrap();
+                },
+            )
+            .await
+            .unwrap();
+        }
+        Commands::Scan { timeout } => {
+            eprintln!("name, mac, rssi");
+            scan_for(
+                Duration::from_millis((timeout * 1000.0) as u64),
+                999,
+                &async |device| -> Result<(), UpdateTargetError> {
+                    let address = device.address();
+                    let update_target = UpdateTarget::new_from_peripheral(&device).await?;
+                    let rssi = device.rssi().await?;
+
+                    let name = update_target.get_name().await?;
+                    println!("{}, {}, {}", name, address, rssi.unwrap_or(-200));
+                    return Ok(());
+                    // update_target.device.disconnect().await.unwrap();
+                },
+            )
+            .await
+            .unwrap();
+        }
     };
-
-    let file_content = tokio::fs::read(file)
-        .await
-        .expect("Failed to read the WASM file");
-
-    scan_for(
-        Duration::from_millis((timeout * 1000.0) as u64),
-        devices,
-        &async |device| -> Result<(), UpdateTargetError> {
-            let update_target = UpdateTarget::new_from_peripheral(device).await?;
-
-            let data = &file_content;
-
-            let now = Instant::now();
-            update_target.write_file(&data).await?;
-            let duration = now.elapsed();
-            println!(
-                "Sending {}k took {} millis",
-                data.len() / 1024,
-                duration.as_millis()
-            );
-            println!(
-                "Thats {}kb/s",
-                (data.len() as f64 / duration.as_millis() as f64)
-            );
-            return Ok(());
-            // update_target.device.disconnect().await.unwrap();
-        },
-    )
-    .await
-    .unwrap();
 
     // sleep(Duration::from_secs(1)).await;
     Ok(())
--- rudelblinken-cli/src/update_target.rs
+++ rudelblinken-cli/src/update_target.rs
@@ -16,6 +16,10 @@ const FILE_UPLOAD_SERVICE_CHECKSUMS: u16 = 0x7895;
 const FILE_UPLOAD_SERVICE_LENGTH: u16 = 0x7896;
 const FILE_UPLOAD_SERVICE_CHUNK_LENGTH: u16 = 0x7897;
 
+const CAT_MANAGEMENT_SERVICE: u16 = 0x7992;
+const CAT_MANAGEMENT_SERVICE_PROGRAM_HASH: u16 = 0x7893;
+const CAT_MANAGEMENT_SERVICE_NAME: u16 = 0x7894;
+
 #[derive(Error, Debug)]
 pub enum UpdateTargetError {
     #[error("BlueR error")]
@@ -36,13 +40,13 @@ pub enum UpdateTargetError {
 pub enum FindUpdateServiceError {
     #[error("BlueR error")]
     BluerError(#[from] bluer::Error),
-    #[error("Does not contain an update service")]
+    #[error("Does not contain the requested service")]
     NoUpdateService,
 }
 
-pub async fn find_update_service(device: &Device) -> Result<Service, FindUpdateServiceError> {
+pub async fn find_service(device: &Device, uuid: u16) -> Result<Service, FindUpdateServiceError> {
     for service in device.services().await? {
-        if service.uuid().await? == uuid::Uuid::from_u16(FILE_UPLOAD_SERVICE) {
+        if service.uuid().await? == uuid::Uuid::from_u16(uuid) {
             return Ok(service);
         }
     }
@@ -77,19 +81,22 @@ pub struct UpdateTarget {
     checksums_characteristic: Characteristic,
     length_characteristic: Characteristic,
     chunk_length_characteristic: Characteristic,
+
+    program_hash_characteristic: Characteristic,
+    name_characteristic: Characteristic,
 }
 
 impl UpdateTarget {
-    pub async fn new_from_peripheral(device: Device) -> Result<UpdateTarget, UpdateTargetError> {
+    pub async fn new_from_peripheral(device: &Device) -> Result<UpdateTarget, UpdateTargetError> {
         let address = device.address();
         // println!("Checking {}", address);
         if !(address.0.starts_with(&[0x24, 0xec, 0x4b])) {
             return Err(UpdateTargetError::MacDoesNotLookLikeAnUpdateTarget);
         }
-        println!("Found MAC {}", address);
+        // println!("Found MAC {}", address);
 
         if !device.is_connected().await? {
-            println!("Connecting...");
+            // println!("Connecting...");
             for attempt in 0..=2 {
                 match device.connect().await {
                     Ok(()) => break,
@@ -100,18 +107,15 @@ impl UpdateTarget {
                         break;
                     }
                     Err(err) => {
-                        println!("Connect error: {}", &err);
+                        eprintln!("Connect error: {}", &err);
                     }
                 }
             }
-        } else {
-            println!("Already connected");
         }
-        println!("Connected to {}", address);
 
         // // // Sometimes this is required to actually discover services
-        let update_service = find_update_service(&device).await?;
-        println!("Found service UUID for {}", address);
+        let update_service = find_service(&device, FILE_UPLOAD_SERVICE).await?;
+        // println!("Found service UUID for {}", address);
 
         let data_characteristic =
             find_characteristic(&update_service, FILE_UPLOAD_SERVICE_DATA).await?;
@@ -124,17 +128,69 @@ impl UpdateTarget {
         let chunk_length_characteristic =
             find_characteristic(&update_service, FILE_UPLOAD_SERVICE_CHUNK_LENGTH).await?;
 
+        let cat_management_service = find_service(&device, CAT_MANAGEMENT_SERVICE).await?;
+
+        let name_characteristic =
+            find_characteristic(&cat_management_service, CAT_MANAGEMENT_SERVICE_NAME).await?;
+        let program_hash_characteristic =
+            find_characteristic(&cat_management_service, CAT_MANAGEMENT_SERVICE_PROGRAM_HASH)
+                .await?;
+
         return Ok(UpdateTarget {
             data_characteristic,
             hash_characteristic,
             checksums_characteristic,
             length_characteristic,
             chunk_length_characteristic,
+            name_characteristic,
+            program_hash_characteristic,
         });
     }
 
+    pub async fn get_name(&self) -> Result<String, UpdateTargetError> {
+        let name_bytes = self.name_characteristic.read().await?;
+        if name_bytes.len() < 3 || name_bytes.len() > 32 {
+            todo!();
+        }
+        let name = String::from_utf8_lossy(&name_bytes);
+        return Ok(name.to_string());
+    }
+
+    pub async fn set_name(&self, name: String) -> Result<String, UpdateTargetError> {
+        let name_bytes = self.name_characteristic.read().await?;
+        if name_bytes.len() < 3 || name_bytes.len() > 32 {
+            todo!();
+        }
+        let name = String::from_utf8_lossy(&name_bytes);
+        return Ok(name.to_string());
+    }
+
+    pub async fn get_program_hash(&self) -> Result<[u8; 32], UpdateTargetError> {
+        let program_hash = self.program_hash_characteristic.read().await?;
+        let Ok(program_hash): Result<[u8; 32], _> = program_hash.try_into() else {
+            todo!();
+        };
+        return Ok(program_hash);
+    }
+
+    pub async fn run_program(&self, data: &[u8]) -> Result<(), UpdateTargetError> {
+        let program_hash = self.upload_file(data).await?;
+        self.program_hash_characteristic
+            .write_ext(
+                &program_hash,
+                &CharacteristicWriteRequest {
+                    offset: 0,
+                    op_type: bluer::gatt::WriteOp::Reliable,
+                    prepare_authorize: false,
+                    _non_exhaustive: (),
+                },
+            )
+            .await?;
+        return Ok(());
+    }
+
     #[async_recursion]
-    pub async fn write_file(&self, data: &[u8]) -> Result<[u8; 32], UpdateTargetError> {
+    pub async fn upload_file(&self, data: &[u8]) -> Result<[u8; 32], UpdateTargetError> {
         let mut hasher = Sha3_256::new();
         hasher.update(&data);
         // TODO: I am sure there is a better way to convert this into an array but I didnt find it after 10 minutes.
@@ -167,7 +223,7 @@ impl UpdateTarget {
         if checksums_data.len() < 32 {
             self.checksums_characteristic.write(checksums_data).await?;
         } else {
-            let checksums_file_hash = self.write_file(checksums_data).await?;
+            let checksums_file_hash = self.upload_file(checksums_data).await?;
             self.checksums_characteristic
                 .write(&checksums_file_hash)
                 .await?;
