7bfda334751015fa1a1cb4e3e19eca3298dec55a
Zebreus <zebreus@zebre.us>
2024-12-18 23:56:06 +0100

    Prepare rudelblinken-cli for publishing

--- rudelblinken-cli/src/main.rs
+++ rudelblinken-cli/src/main.rs
@@ -1,5 +1,25 @@
+//! # Rudelblinken CLI
+//!
+//! `rudel` is the cli utility for the `rudelblinken` project. It is used to program and run WASM binaries on the `rudelblinken` devices. It can also run WASM binaries in a simulated environment.
+//!
+//! ## Usage
+//!
+//! Until I have time to write proper documentation, here is the output of `rudel --help`:
+//!
+//! ```
+//! Usage: rudel <COMMAND>
+//!
+//! Commands:
+//! upload   Upload a file
+//! run      Run a WASM binary
+//! scan     Scan for cats
+//! emulate  Emulate a rudelblinken device
+//! help     Print this message or the help of the given subcommand(s)
+//!
+//! Options:
+//! -h, --help     Print help
+//! ```
 #![feature(async_closure)]
-//! Connects to our Bluetooth GATT service and exercises the characteristic.
 
 mod bluetooth;
 mod emulator;
