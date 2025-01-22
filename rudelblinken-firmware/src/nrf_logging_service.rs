fa0d35bea61c5a5eb124b03585b5561cae6f312f
Zebreus <zebreus@zebre.us>
2025-01-22 14:02:28 +0100

    Enable logging entered and exited spans

--- rudelblinken-firmware/src/nrf_logging_service.rs
+++ rudelblinken-firmware/src/nrf_logging_service.rs
@@ -10,6 +10,7 @@ use std::{
     sync::{Arc, OnceLock},
     u8,
 };
+use tracing_subscriber::fmt::format::FmtSpan;
 
 // https://docs.nordicsemi.com/bundle/ncs-latest/page/nrf/libraries/bluetooth/services/nus.html#nus-service-readme
 const SERIAL_LOGGING_TIO_SERVICE: BleUuid = uuid128!("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
@@ -217,6 +218,7 @@ impl SerialLoggingService {
         }));
 
         tracing_subscriber::fmt()
+            .with_span_events(FmtSpan::ENTER | FmtSpan::EXIT)
             .with_max_level(tracing::Level::INFO)
             .with_writer(|| SerialWriter {})
             .init();
