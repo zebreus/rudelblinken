c0f759da96f5e9a81107d022a35b047d35b81879
Zebreus <zebreus@zebre.us>
2025-01-24 16:44:51 +0100

    Split up file upload service

---
+++ rudelblinken-firmware/src/file_upload_service/incomplete_file.rs
@@ -0,0 +1,143 @@
+use crate::storage::FlashStorage;
+use itertools::Itertools;
+use rudelblinken_filesystem::{
+    file::{File as FileContent, FileState},
+    Filesystem,
+};
+use std::io::{Seek, Write};
+use thiserror::Error;
+
+#[derive(Debug)]
+pub(super) struct IncompleteFile {
+    incomplete_file: FileContent<FlashStorage, { FileState::Writer }>,
+    checksums: Vec<u8>,
+    received_chunks: Vec<bool>,
+    chunk_length: u16,
+    length: u32,
+    name: String,
+    hash: [u8; 32],
+}
+
+#[derive(Error, Debug, Clone)]
+pub enum ReceiveChunkError {
+    #[error("Chunk has an invalid length")]
+    InvalidLength,
+    #[error("Chunk has the wrong checksum")]
+    WrongChecksum,
+}
+
+#[derive(Error, Debug, Clone)]
+pub enum VerifyFileError {
+    #[error("File is not complete")]
+    NotComplete,
+    #[error("Hashes do not match")]
+    HashMismatch,
+}
+
+impl IncompleteFile {
+    pub fn new(
+        hash: [u8; 32],
+        checksums: Vec<u8>,
+        chunk_length: u16,
+        length: u32,
+        writer: FileContent<FlashStorage, { FileState::Writer }>,
+        name: String,
+    ) -> Self {
+        Self {
+            incomplete_file: writer,
+            received_chunks: vec![false; checksums.len()],
+            checksums,
+            chunk_length,
+            length,
+            name,
+            hash,
+        }
+    }
+
+    pub fn receive_chunk(&mut self, data: &[u8], index: u16) -> Result<(), ReceiveChunkError> {
+        // Verify length for all but the last chunk
+        if (index as usize != self.checksums.len() - 1)
+            && (data.len() != self.chunk_length as usize)
+        {
+            return Err(ReceiveChunkError::InvalidLength);
+        }
+        // Verify length for the last chunk
+        if (index as usize == self.checksums.len() - 1)
+            && (data.len() != (self.length as usize % self.chunk_length as usize))
+        {
+            return Err(ReceiveChunkError::InvalidLength);
+        }
+
+        // TODO: Find out if generating a new crc8 generator costs anything
+        let crc8_generator = crc::Crc::<u8>::new(&crc::CRC_8_LTE);
+        let checksum = crc8_generator.checksum(data);
+
+        if self.checksums[index as usize] != checksum {
+            ::tracing::error!(target: "file-upload", "Received chunk with invalid checksum");
+            return Err(ReceiveChunkError::WrongChecksum);
+        }
+
+        let offset = self.chunk_length as usize * index as usize;
+        self.incomplete_file
+            .seek(std::io::SeekFrom::Start(offset as u64))
+            .unwrap();
+        self.incomplete_file.write(data).unwrap();
+        // self.incomplete_file.content[offset..(data.len() + offset)].copy_from_slice(data);
+        self.received_chunks[index as usize] = true;
+
+        Ok(())
+    }
+    /// Get all chunks that have not yet been received
+    pub fn get_missing_chunks(&self) -> Vec<u16> {
+        self.received_chunks
+            .iter()
+            .enumerate()
+            .filter(|(_, received)| received == &&false)
+            .map(|(index, _)| index as u16)
+            .collect_vec()
+    }
+    pub fn chunk_count(&self) -> u16 {
+        self.received_chunks.len() as u16
+    }
+    /// Check if the file is complete
+    pub fn is_complete(&self) -> bool {
+        self.received_chunks.iter().all(|received| *received)
+    }
+    /// Verify that the received file is complete and has the correct hash
+    pub fn verify_hash(
+        self,
+        filesystem: &Filesystem<FlashStorage>,
+    ) -> Result<FileContent<FlashStorage, { FileState::Weak }>, VerifyFileError> {
+        if !self.is_complete() {
+            return Err(VerifyFileError::NotComplete);
+        }
+        self.incomplete_file.commit().unwrap();
+        let file = filesystem.read_file(&self.name).unwrap();
+        let mut hasher = blake3::Hasher::new();
+        hasher.update(file.upgrade().unwrap().as_ref());
+
+        // TODO: I am sure there is a better way to convert this into an array but I didnt find it after 10 minutes.
+        let mut hash: [u8; 32] = [0; 32];
+        hash.copy_from_slice(hasher.finalize().as_bytes());
+
+        if hash != self.hash {
+            ::tracing::warn!(target: "file-upload", "Hashes dont match.\nExpected: {:?}\nGot     : {:?}", self.hash, hash);
+            return Err(VerifyFileError::HashMismatch);
+        }
+        ::tracing::info!(target: "file-upload", "Hashes match");
+
+        Ok(file)
+    }
+    /// Get the uploaded file, if the upload is finished, otherwise this return None and you just destroyed your incomplete file for no reason
+    pub fn into_file(
+        self,
+        filesystem: &Filesystem<FlashStorage>,
+    ) -> Result<FileContent<FlashStorage, { FileState::Weak }>, VerifyFileError> {
+        let file = self.verify_hash(filesystem)?;
+        Ok(file)
+    }
+
+    pub fn get_hash(&self) -> &[u8; 32] {
+        &self.hash
+    }
+}
---
+++ rudelblinken-firmware/src/file_upload_service/low_level.rs
@@ -0,0 +1,212 @@
+use crate::service_helpers::DocumentableCharacteristic;
+use esp32_nimble::{
+    utilities::{mutex::Mutex, BleUuid},
+    BLE2904Format, BLEServer, BLEService, NimbleProperties,
+};
+use esp_idf_sys::{ble_svc_gatt_changed, BLE_GATT_CHR_UNIT_UNITLESS};
+use std::sync::Arc;
+
+use super::FileUploadService;
+
+const FILE_UPLOAD_SERVICE: u16 = 0x9160;
+// Write data chunks here
+const FILE_UPLOAD_SERVICE_DATA: u16 = 0x9161;
+// Write metadata here to initiate an upload.
+const FILE_UPLOAD_SERVICE_START_UPLOAD: u16 = 0x9162;
+// Read this to get the number of uploaded chunks and the IDs of some missing chunks. Returns a list of u16
+const FILE_UPLOAD_SERVICE_UPLOAD_PROGRESS: u16 = 0x9163;
+// Read here to get the last error as a string
+const FILE_UPLOAD_SERVICE_LAST_ERROR: u16 = 0x9164;
+// Read to get the hash of the current upload.
+const FILE_UPLOAD_SERVICE_CURRENT_HASH: u16 = 0x9166;
+
+const FILE_UPLOAD_SERVICE_UUID: BleUuid = BleUuid::from_uuid16(FILE_UPLOAD_SERVICE);
+const FILE_UPLOAD_SERVICE_DATA_UUID: BleUuid = BleUuid::from_uuid16(FILE_UPLOAD_SERVICE_DATA);
+const FILE_UPLOAD_SERVICE_START_UPLOAD_UUID: BleUuid =
+    BleUuid::from_uuid16(FILE_UPLOAD_SERVICE_START_UPLOAD);
+const FILE_UPLOAD_SERVICE_MISSING_CHUNKS_UUID: BleUuid =
+    BleUuid::from_uuid16(FILE_UPLOAD_SERVICE_UPLOAD_PROGRESS);
+const FILE_UPLOAD_SERVICE_LAST_ERROR_UUID: BleUuid =
+    BleUuid::from_uuid16(FILE_UPLOAD_SERVICE_LAST_ERROR);
+const FILE_UPLOAD_SERVICE_CURRENT_HASH_UUID: BleUuid =
+    BleUuid::from_uuid16(FILE_UPLOAD_SERVICE_CURRENT_HASH);
+
+fn setup_service(server: &mut BLEServer) -> Arc<Mutex<BLEService>> {
+    server.create_service(FILE_UPLOAD_SERVICE_UUID)
+}
+
+fn setup_data_characteristic(
+    service: &Arc<Mutex<BLEService>>,
+    file_upload_service: &Arc<Mutex<FileUploadService>>,
+) {
+    let data_characteristic = service.lock().create_characteristic(
+        FILE_UPLOAD_SERVICE_DATA_UUID,
+        NimbleProperties::WRITE_NO_RSP | NimbleProperties::WRITE,
+    );
+    data_characteristic.document(
+        "Chunk Upload",
+        BLE2904Format::OPAQUE,
+        0,
+        BLE_GATT_CHR_UNIT_UNITLESS,
+    );
+
+    let file_upload_service_clone = file_upload_service.clone();
+    data_characteristic.lock().on_write(move |args| {
+        let mut service = file_upload_service_clone.lock();
+        if let Err(e) = service.data_write(args) {
+            service.log_error(e);
+        }
+    });
+}
+
+fn setup_upload_request_characteristic(
+    service: &Arc<Mutex<BLEService>>,
+    file_upload_service: &Arc<Mutex<FileUploadService>>,
+) {
+    // Write a upload request to start a new upload.
+    // Read to get the hash of the current upload.
+    let upload_request_characteristic = service.lock().create_characteristic(
+        FILE_UPLOAD_SERVICE_START_UPLOAD_UUID,
+        NimbleProperties::READ | NimbleProperties::WRITE,
+    );
+    upload_request_characteristic.document(
+        "File Upload Request",
+        BLE2904Format::OPAQUE,
+        0,
+        BLE_GATT_CHR_UNIT_UNITLESS,
+    );
+
+    let file_upload_service_clone = file_upload_service.clone();
+    upload_request_characteristic.lock().on_write(move |args| {
+        println!("Writing upload request");
+        let mut service = file_upload_service_clone.lock();
+        if let Err(e) = service.request_upload(args) {
+            service.log_error(e);
+        }
+        unsafe {
+            ble_svc_gatt_changed(FILE_UPLOAD_SERVICE_DATA, FILE_UPLOAD_SERVICE_DATA);
+        };
+    });
+}
+
+fn setup_current_hash_characteristic(
+    service: &Arc<Mutex<BLEService>>,
+    file_upload_service: &Arc<Mutex<FileUploadService>>,
+) {
+    let current_hash_characteristic = service.lock().create_characteristic(
+        FILE_UPLOAD_SERVICE_CURRENT_HASH_UUID,
+        NimbleProperties::READ,
+    );
+    current_hash_characteristic.document(
+        "Hash of the current upload",
+        BLE2904Format::OPAQUE,
+        0,
+        BLE_GATT_CHR_UNIT_UNITLESS,
+    );
+
+    let file_upload_service_clone = file_upload_service.clone();
+    current_hash_characteristic.lock().on_read(move |value, _| {
+        println!("Read current hash");
+        let service = file_upload_service_clone.lock();
+        let current_hash = match &service.currently_receiving {
+            Some(currently_receiving) => currently_receiving.get_hash(),
+            None => &[0u8; 32],
+        };
+        value.set_value(current_hash);
+    });
+}
+
+fn setup_upload_status_characteristic(
+    service: &Arc<Mutex<BLEService>>,
+    file_upload_service: &Arc<Mutex<FileUploadService>>,
+) {
+    let upload_status_characteristic = service.lock().create_characteristic(
+        FILE_UPLOAD_SERVICE_MISSING_CHUNKS_UUID,
+        NimbleProperties::READ,
+    );
+    upload_status_characteristic.document(
+        "Number of received chunks + Missing Chunks",
+        BLE2904Format::OPAQUE,
+        0,
+        BLE_GATT_CHR_UNIT_UNITLESS,
+    );
+
+    let file_upload_service_clone = file_upload_service.clone();
+    upload_status_characteristic
+        .lock()
+        .on_read(move |value, _| {
+            println!("Reading missing chunks");
+            let service = file_upload_service_clone.lock();
+            let maybe_currently_receiving = service.currently_receiving.as_ref();
+            let missing_chunks = maybe_currently_receiving
+                .map(|incomplete_file| incomplete_file.get_missing_chunks())
+                .unwrap_or(Default::default());
+
+            let chunk_count = maybe_currently_receiving
+                .map(|incomplete_file| incomplete_file.chunk_count())
+                .unwrap_or(0);
+            let progress = chunk_count - missing_chunks.len() as u16;
+
+            let mut upload_status: Vec<u8> = Vec::new();
+            upload_status.extend_from_slice(&progress.to_le_bytes());
+            upload_status.extend(
+                missing_chunks
+                    .into_iter()
+                    .take(100)
+                    .flat_map(u16::to_le_bytes),
+            );
+
+            value.set_value(&upload_status);
+        });
+}
+
+fn setup_last_error_characteristic(
+    service: &Arc<Mutex<BLEService>>,
+    file_upload_service: &Arc<Mutex<FileUploadService>>,
+) {
+    let last_error_characteristic = service
+        .lock()
+        .create_characteristic(FILE_UPLOAD_SERVICE_LAST_ERROR_UUID, NimbleProperties::READ);
+    last_error_characteristic.document(
+        "Last error code",
+        BLE2904Format::UINT16,
+        0,
+        BLE_GATT_CHR_UNIT_UNITLESS,
+    );
+
+    let file_upload_service_clone = file_upload_service.clone();
+    last_error_characteristic.lock().on_read(move |value, _| {
+        let service = file_upload_service_clone.lock();
+        let Some(last_error) = &service.last_error else {
+            value.set_value(&[]);
+            return;
+        };
+
+        value.set_value(&(unsafe { *<*const _>::from(last_error).cast::<u8>() }).to_le_bytes());
+    });
+}
+
+impl FileUploadService {
+    // TODO: We should only allow one active upload service at a time.
+    /// Create a new FileUploadService and set up the necessary characteristics.
+    pub fn new(server: &mut BLEServer) -> Arc<Mutex<Self>> {
+        let file_upload_service = Arc::new(Mutex::new(FileUploadService {
+            currently_receiving: None,
+            last_error: None,
+        }));
+
+        let service = setup_service(server);
+        setup_data_characteristic(&service, &file_upload_service);
+        setup_upload_request_characteristic(&service, &file_upload_service);
+        setup_current_hash_characteristic(&service, &file_upload_service);
+        setup_upload_status_characteristic(&service, &file_upload_service);
+        setup_last_error_characteristic(&service, &file_upload_service);
+
+        file_upload_service
+    }
+
+    /// Get the UUID of the file upload service
+    pub const fn uuid() -> BleUuid {
+        FILE_UPLOAD_SERVICE_UUID
+    }
+}
