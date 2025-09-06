1268c14a2e575aead6987bdc159b958fdc1f36f9
Zebreus <zebreus@zebre.us>
2025-09-06 20:36:19 +0200

    Improve board test

--- wasm-binaries/board-test/src/lib.rs
+++ wasm-binaries/board-test/src/lib.rs
@@ -1,6 +1,8 @@
-use rudelblinken_sdk::{get_voltage, log, set_leds, time, yield_now, Advertisement, LogLevel};
+use rudelblinken_sdk::{
+    get_ambient_light, get_voltage, log, set_leds, time, yield_now, Advertisement, LogLevel,
+};
 use spin::Once;
-use std::sync::{LazyLock, Mutex};
+use std::sync::{Arc, LazyLock, Mutex};
 
 static ADVERTISMENT_COUNTER: LazyLock<Mutex<u32>> = LazyLock::new(|| Mutex::new(0));
 
@@ -17,9 +19,117 @@ fn measure_voltage() -> u16 {
 static USB_SUPPLY_WORKING: Once<bool> = Once::new();
 static BATTERY_SUPPLY_WORKING: Once<bool> = Once::new();
 static BLE_WORKING: Once<bool> = Once::new();
+static AMBIENT_WORKING: Once<bool> = Once::new();
+
+static RESULT_PRINTED: Once<bool> = Once::new();
 
 const BLE_WORKING_DURATION: u64 = 10 * 1000 * 1000;
 const BLE_WORKING_THRESHOLD: u32 = 10;
+
+const AMBIENT_PHASE_DURATION: u32 = 3;
+static AMBIENT_TEST_STATE_: LazyLock<Arc<Mutex<AmbientTestState>>> =
+    LazyLock::new(|| Arc::new(Mutex::new(AmbientTestState::Low(0))));
+enum AmbientTestState {
+    Low(u32),
+    High(u32),
+    LowAgain(u32),
+    HighAgain(u32),
+}
+
+fn test_ambient() {
+    let ambient = get_ambient_light();
+    if AMBIENT_WORKING.get().is_some() {
+        return;
+    }
+
+    let mut state = AMBIENT_TEST_STATE_.lock().unwrap();
+    let new_state = match &mut *state {
+        AmbientTestState::Low(counter) => {
+            if ambient < 5 {
+                if *counter == AMBIENT_PHASE_DURATION {
+                    log(
+                        LogLevel::Info,
+                        "[1/4] Please shine light on the sensor to start the test",
+                    );
+                }
+                AmbientTestState::Low(*counter + 1)
+            } else {
+                if *counter >= AMBIENT_PHASE_DURATION {
+                    AmbientTestState::High(0)
+                } else {
+                    AmbientTestState::Low(0)
+                }
+            }
+        }
+        AmbientTestState::High(counter) => {
+            if ambient >= 5 {
+                if *counter == AMBIENT_PHASE_DURATION {
+                    log(
+                        LogLevel::Info,
+                        "[2/4] Cover the sensor again to continue the test",
+                    );
+                }
+                AmbientTestState::High(*counter + 1)
+            } else {
+                if *counter >= AMBIENT_PHASE_DURATION {
+                    AmbientTestState::LowAgain(0)
+                } else {
+                    log(
+                        LogLevel::Info,
+                        "Ambient light sensor test failed, restarting",
+                    );
+                    AmbientTestState::Low(0)
+                }
+            }
+        }
+        AmbientTestState::LowAgain(counter) => {
+            if ambient < 5 {
+                if *counter == AMBIENT_PHASE_DURATION {
+                    log(
+                        LogLevel::Info,
+                        "[3/4] Shine light on the sensor again to finish the test",
+                    );
+                }
+                AmbientTestState::LowAgain(*counter + 1)
+            } else {
+                if *counter >= AMBIENT_PHASE_DURATION {
+                    AmbientTestState::HighAgain(0)
+                } else {
+                    log(
+                        LogLevel::Info,
+                        "Ambient light sensor test failed, restarting",
+                    );
+                    AmbientTestState::Low(0)
+                }
+            }
+        }
+        AmbientTestState::HighAgain(counter) => {
+            if *counter >= AMBIENT_PHASE_DURATION {
+                AMBIENT_WORKING.call_once(|| {
+                    log(LogLevel::Info, "✅: Ambient light sensor working");
+                    true
+                });
+            }
+            if ambient >= 5 {
+                AmbientTestState::HighAgain(*counter + 1)
+            } else {
+                log(
+                    LogLevel::Info,
+                    "Ambient light sensor test failed, restarting",
+                );
+                AmbientTestState::Low(0)
+            }
+        }
+    };
+
+    *state = new_state;
+}
+
+// fn test_microphone() {
+//     // let ambient = ();
+
+//     log(LogLevel::Info, &format!("Ambient: {}", ambient));
+// }
 
 fn test_voltage() {
     let voltage = measure_voltage();
@@ -114,11 +224,41 @@ fn main() {
 
         test_voltage();
         test_ble();
+        test_ambient();
 
         set_leds(0, &[255]);
-        yield_now(1000 * 200);
+        yield_now(1000 * 300);
+
+        test_ambient();
+
         set_leds(0, &[0]);
         yield_now(1000 * 300);
+
+        if let (Some(ble), Some(ambient), Some(voltage)) = (
+            BLE_WORKING.get(),
+            AMBIENT_WORKING.get(),
+            USB_SUPPLY_WORKING.get(),
+        ) {
+            if RESULT_PRINTED.get().is_some() {
+                continue;
+            }
+            RESULT_PRINTED.call_once(|| {
+                if *ble && *ambient && *voltage {
+                    log(LogLevel::Info, "🎉 All automated tests passed!");
+                    log(LogLevel::Info, "(You need to test the LED strip manually)");
+                } else {
+                    log(
+                        LogLevel::Info,
+                        "Some tests failed, please see above for details",
+                    );
+                    log(
+                        LogLevel::Info,
+                        "You can restart the test by resetting the board",
+                    );
+                }
+                true
+            });
+        }
     }
 }
 
