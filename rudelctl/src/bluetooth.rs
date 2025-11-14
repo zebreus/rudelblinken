27d4851575a74d7095ec3ee8b9683c99c0c7d61a
redsolver <red@s5.pro>
2025-11-14 22:29:21 +0100

    replace continue with break

--- rudelctl/src/bluetooth.rs
+++ rudelctl/src/bluetooth.rs
@@ -1,3 +1,4 @@
+use bluer::monitor::{Monitor, MonitorEvent, Pattern, RssiSamplingPeriod, Type as MonitorType};
 use bluer::DiscoveryFilter;
 use futures::{
     pin_mut,
@@ -8,111 +9,9 @@ use futures_time::stream::StreamExt;
 use futures_time::time::Duration;
 use std::{collections::HashSet, future::Future};
 
-// pub enum BluetoothError {
-//     BluerError(bluer::Error),
-// }
-
-// pub enum Device {
-//     Ble { device: bluer::Device },
-//     Simulated { address: Address },
-// }
-
-// impl Device {
-//     pub fn address(&self) -> Result<Address, BluetoothError> {
-//         match self {
-//             Device::Ble { device } => device.address().into(),
-//             Device::Simulated { address } => address.clone(),
-//         }
-//     }
-//     pub async fn is_connected(&self) -> Result<bool, BluetoothError> {
-//         match self {
-//             Device::Ble { device } => device.is_connected().await?,
-//             Device::Simulated { .. } => true,
-//         }
-//     }
-// }
-
-// /// Bluetooth address. Copied from bluer.
-// ///
-// /// The serialized representation is a string in colon-hexadecimal notation.
-// #[derive(Clone, Copy, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
-// pub struct Address(pub [u8; 6]);
-// impl Address {
-//     /// Creates a new Bluetooth address with the specified value.
-//     pub const fn new(addr: [u8; 6]) -> Self {
-//         Self(addr)
-//     }
-
-//     /// Any Bluetooth address.
-//     ///
-//     /// Corresponds to `00:00:00:00:00:00`.
-//     pub const fn any() -> Self {
-//         Self([0; 6])
-//     }
-// }
-// impl Deref for Address {
-//     type Target = [u8; 6];
-
-//     fn deref(&self) -> &Self::Target {
-//         &self.0
-//     }
-// }
-// impl DerefMut for Address {
-//     fn deref_mut(&mut self) -> &mut Self::Target {
-//         &mut self.0
-//     }
-// }
-// impl Display for Address {
-//     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-//         write!(
-//             f,
-//             "{:02X}:{:02X}:{:02X}:{:02X}:{:02X}:{:02X}",
-//             self.0[0], self.0[1], self.0[2], self.0[3], self.0[4], self.0[5]
-//         )
-//     }
-// }
-// impl Debug for Address {
-//     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-//         write!(f, "{self}")
-//     }
-// }
-// impl From<bluer::Address> for Address {
-//     fn from(addr: bluer::Address) -> Self {
-//         Self(addr.0)
-//     }
-// }
-// impl Into<bluer::Address> for Address {
-//     fn into(self) -> bluer::Address {
-//         bluer::Address(self.0)
-//     }
-// }
-
-// struct Address {
-//     address: bluer::Address,
-// }
-
-// struct MyDevice {
-//     device: bluer::Device,
-// }
-
-// impl MyDevice {
-//     pub fn address(&self) -> Result<Address, BluetoothError> {
-//         self.device.address().into()
-//     }
-//     pub async fn is_connected(&self) -> Result<bool, BluetoothError> {
-//         self.device.is_connected().await?
-//     }
-// }
-
-// struct MyDescriptor {
-//     descriptor: bluer::Descriptor,
-// }
-
 #[derive(Debug)]
 pub enum Outcome {
-    // Processed device
     Processed,
-    // Ignored device, but continue scanning
     Ignored,
 }
 
@@ -120,7 +19,6 @@ pub async fn scan_for<Fut, Err>(
     duration: Duration,
     // Just give a big number if you dont want a limit
     max_devices: u32,
-    // Filter devices by name
     name_filter: impl Fn(&str) -> bool,
     // Power cycle the adapter to make discovery more reliable
     // TODO: Find a better fix
@@ -132,15 +30,75 @@ where
     Fut: Future<Output = Result<Outcome, Err>>,
 {
     let session = bluer::Session::new().await?;
-
     let adapter = session.default_adapter().await?;
 
     // Power cycle the adapter to make discovery more reliable
     if powercycle_adapter {
-        adapter.set_powered(false).await?;
+        let _ = adapter.set_powered(false).await;
     }
     adapter.set_powered(true).await?;
 
+    /// !! AI Warning OwO (generated and untested, likely broken)
+    /*  // Try Advertisement Monitor first (more reliable and passive on BlueZ)
+    if let Ok(mm) = adapter.monitor().await {
+        let name_prefix = b"[rb]".to_vec();
+        let pattern_complete = Pattern {
+            data_type: 0x09,
+            start_position: 0,
+            content: name_prefix.clone(),
+        };
+        let pattern_short = Pattern {
+            data_type: 0x08,
+            start_position: 0,
+            content: name_prefix.clone(),
+        };
+        let mut handle = mm
+            .register(Monitor {
+                monitor_type: MonitorType::OrPatterns,
+                rssi_low_threshold: None,
+                rssi_high_threshold: None,
+                rssi_low_timeout: None,
+                rssi_high_timeout: None,
+                rssi_sampling_period: Some(RssiSamplingPeriod::First),
+                patterns: Some(vec![pattern_complete, pattern_short]),
+                ..Default::default()
+            })
+            .await?;
+
+        let (abort_handle, abort_registration) = AbortHandle::new_pair();
+        let stream = Abortable::new(&mut handle, abort_registration);
+        pin_mut!(stream);
+        let mut stream = stream.timeout(duration);
+
+        let mut programmed_devices = 0u32;
+        while let Some(evt) = stream.next().await {
+            let Ok(evt) = evt else {
+                break;
+            };
+            if let MonitorEvent::DeviceFound(devid) = evt {
+                let device = adapter.device(devid.device)?;
+                // Already filtered by AD name pattern; try processing immediately
+                let result = f(device, abort_handle.clone()).await;
+                if let Err(error) = result {
+                    let string_error = format!("{:?}", error);
+                    if !string_error.contains("TargetDoesNotLookLikeAnUploadServiceProvider") {
+                        log::error!("Failed processing device with {:?}", string_error);
+                    }
+                    continue;
+                }
+                if let Ok(Outcome::Processed) = result {
+                    programmed_devices += 1;
+                    if programmed_devices >= max_devices {
+                        abort_handle.abort();
+                        break;
+                    }
+                }
+            }
+        }
+        drop(handle);
+        return Ok(());
+    } */
+    // Fallback: classic discovery stream
     let filter = DiscoveryFilter {
         uuids: HashSet::new(),
         rssi: None,
@@ -154,55 +112,53 @@ where
     // This is allowed to fail, as filters are not reliable anyways
     let _ = adapter.set_discovery_filter(filter).await;
 
-    {
-        // Starts a discovery session
-        // Monitor would be way more appropriate here, but that requires the user to enable experimental features in their bluetoothd
-        let discover = adapter.discover_devices().await?;
-        pin_mut!(discover);
-        let (abort_handle, abort_registration) = AbortHandle::new_pair();
-        let stream = Abortable::new(discover, abort_registration);
-        let mut stream = stream.timeout(duration);
-        let mut programmed_devices = 0;
-        while let Some(evt) = stream.next().await {
-            let Ok(evt) = evt else {
-                break;
-            };
-            match evt {
-                bluer::AdapterEvent::PropertyChanged(a) => {
-                    log::debug!("Adapter property changed: {:?}", a);
+    // Starts a discovery session
+    // Monitor would be way more appropriate here, but that requires the user to enable experimental features in their bluetoothd
+    let discover = adapter.discover_devices().await?;
+    pin_mut!(discover);
+    let (abort_handle, abort_registration) = AbortHandle::new_pair();
+    let stream = Abortable::new(discover, abort_registration);
+    let mut stream = stream.timeout(duration);
+    let mut programmed_devices = 0;
+    while let Some(evt) = stream.next().await {
+        let Ok(evt) = evt else {
+            break;
+        };
+        match evt {
+            bluer::AdapterEvent::PropertyChanged(a) => {
+                log::debug!("Adapter property changed: {:?}", a);
+            }
+            bluer::AdapterEvent::DeviceRemoved(addr) => {
+                log::debug!("Device removed: {:?}", addr);
+            }
+            bluer::AdapterEvent::DeviceAdded(addr) => {
+                let device = adapter.device(addr)?;
+                let Some(name) = device.name().await? else {
+                    continue;
+                };
+                if !name_filter(&name) {
+                    continue;
                 }
-                bluer::AdapterEvent::DeviceRemoved(addr) => {
-                    log::debug!("Device removed: {:?}", addr);
-                }
-                bluer::AdapterEvent::DeviceAdded(addr) => {
-                    let device = adapter.device(addr)?;
-                    let Some(name) = device.name().await? else {
-                        continue;
-                    };
-                    if !name_filter(&name) {
-                        continue;
-                    }
 
-                    let result = f(device, abort_handle.clone()).await;
-                    if let Err(error) = result {
-                        let string_error = format!("{:?}", error);
-                        if !string_error.contains("TargetDoesNotLookLikeAnUploadServiceProvider") {
-                            log::error!("Failed processing device with {:?}", string_error);
-                        }
-                        continue;
+                let result = f(device, abort_handle.clone()).await;
+                if let Err(error) = result {
+                    let string_error = format!("{:?}", error);
+                    if !string_error.contains("TargetDoesNotLookLikeAnUploadServiceProvider") {
+                        log::error!("Failed processing device with {:?}", string_error);
                     }
-                    if let Ok(Outcome::Processed) = result {
-                        programmed_devices += 1;
-                        if programmed_devices >= max_devices {
-                            // log::info!("Done after programming {} devices", max_devices);
-                            abort_handle.abort();
-                            continue;
-                        }
+                    continue;
+                }
+                if let Ok(Outcome::Processed) = result {
+                    programmed_devices += 1;
+                    if programmed_devices >= max_devices {
+                        // log::info!("Done after programming {} devices", max_devices);
+                        abort_handle.abort();
+                        break;
                     }
                 }
             }
         }
     }
 
-    return Ok(());
+    Ok(())
 }
